aae1d2883133e63ce9c7524c25e38e5d3f005337
ungsochea <sochea.ung@gmail.com>
2019-09-24 16:50:39 +0700

    migrate auth with change uuid

---
+++ resources/js/app.js
@@ -0,0 +1,11 @@
+require('./bootstrap');
+
+window.Vue = require('vue');
+ files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+
+const app = new Vue({
+    el: '#app',
+});
